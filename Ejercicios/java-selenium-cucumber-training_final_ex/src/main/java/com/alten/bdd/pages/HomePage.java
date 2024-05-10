a6d9b25e66cc7ff29084cac7fc4a81e5d3a3101e
maalvrei <miguemorisco@gmail.com>
2024-05-10 10:13:37 +0200

    añadido ejemplo trabajo final

---
+++ Ejercicios/java-selenium-cucumber-training_final_ex/src/main/java/com/alten/bdd/pages/HomePage.java
@@ -0,0 +1,25 @@
+package com.alten.bdd.pages;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class HomePage extends BasePage{
+	
+	private static final Logger LOGGER = LogManager.getLogger(HomePage.class);
+	private static final By IMDB_SIGNIN_LINK 	= By.className("imdb-header__signin-text");
+	private static final By IMDB_HOME_PAGE_LOGO	= By.id("home_img_holder");
+
+	
+	public void verifyHomePageIsLoaded() {
+		LOGGER.info("verifyHomePageIsLoaded");
+		this.waitToElementBeClickable(IMDB_HOME_PAGE_LOGO);
+	}
+	
+	public void navigateToSignInPage() {
+		LOGGER.info("navigateToSignInPage");
+		WebElement element = getDriver().findElement(IMDB_SIGNIN_LINK);
+		element.click();
+	}
+}
---
+++ Ejercicios/java-selenium-cucumber-training_final_ex/src/test/resources/cucumber/imdb_oscars.feature
@@ -0,0 +1,17 @@
+@Oscars
+Feature: IMDB Search actor
+  As a user
+  I want to search an Actor was nominated to best actor
+
+@nominatedToBestActor
+Scenario Outline: Search Actor and Film
+	Given IMDB home page is showed
+	When I click menu 
+	And select Oscar Awards
+	Then I navigate to Oscar <age>
+	When best actor section is showed
+	Then <actor> and <film> was displayed
+	And  verify if he won the oscar <winner>
+ Examples:
+    | 	film 																										| age		| actor 			| winner |
+    | 	Piratas del Caribe: la maldición de la Perla Negra 			| 2004 	| Johnny Deep | false  |
